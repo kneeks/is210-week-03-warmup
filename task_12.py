ee4ee252cf580b328b34f7dafaaba2572d2cfde7
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2015-09-10 22:06:57 +0000

    assigned numeric

---
+++ task_12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Assigns simple numeric types."""
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
